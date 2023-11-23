0423d66321e8d62a935b559a15583079e939d2cc
Shubham Parkhi <shubhamparkhi10@gmail.com>
2023-11-23 15:38:50 +0530

    Minor improvements

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -1,9 +1,9 @@
-import SocialHebbalContainer from "../components/social-hebbal-container";
+import Admin from "../components/Admin";
 
 const Desktop2 = () => {
   return (
     <div className="relative bg-gray w-full h-[100vh] overflow-y-auto flex flex-col items-center justify-center">
-      <SocialHebbalContainer />
+      <Admin />
     </div>
   );
 };
